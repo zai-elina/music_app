8bd60028d69b1b12406f3462b4e4e4df0da472ed
Элина <@COMPUTER>
2023-07-05 17:05:40 +0300

    The left menu opens when you click on a burger

--- src/components/layout/navigation/NavBurger/NavBurger.js
+++ src/components/layout/navigation/NavBurger/NavBurger.js
@@ -1,9 +1,21 @@
+import { useState } from 'react'
+import Menu from '../Menu/Menu'
+
 export default function NavBurger() {
+  const [visible, setVisible] = useState(false)
+
+  const toogleVisibility = () => {
+    setVisible(!visible)
+  }
+
   return (
-    <div className="nav__burger burger">
-      <span className="burger__line"></span>
-      <span className="burger__line"></span>
-      <span className="burger__line"></span>
+    <div>
+      <div className="nav__burger burger" onClick={toogleVisibility}>
+        <span className="burger__line"></span>
+        <span className="burger__line"></span>
+        <span className="burger__line"></span>
+      </div>
+      {visible && <Menu />}
     </div>
   )
 }
