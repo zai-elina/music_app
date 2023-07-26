6b7c844cad805f114e91bffa8055bb3a4d109ce3
Elina <8gelina@gmail.com>
2023-07-26 22:58:11 +0400

    changes in function getTracks

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
 export async function getTracks() {
-    const response = await fetch('https://painassasin.online/catalog/track/all/')
-    .catch((error) =>
-        {throw new Error("Не удалось загрузить плейлист, попробуйте позже")}
-    );
+    try {
+        const response = await fetch('https://painassasin.online/catalog/track/all/');
+        const data = await response.json();
 
-    const data = await response.json();
-    return data;
+        return data;
+    } catch {
+        throw new Error("Не удалось загрузить плейлист, попробуйте позже");
+    }
  }
