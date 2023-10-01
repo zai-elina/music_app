f978320975391ce19c8efc67f8dbc6ac7f97b61e
Элина <@COMPUTER>
2023-10-01 17:01:45 +0300

    change selectors

--- src/store/selectors/tracksSelector.js
+++ src/store/selectors/tracksSelector.js
@@ -1,7 +1,7 @@
-export const playlistSelector = (store) => store.audioplayer.playlist
+export const playlistSelector = (state) => state.audioplayer.playlist
 
-export const shufflePlaylistSelector = (store) => store.audioplayer.shufflePlaylist
+export const shufflePlaylistSelector = (state) => state.audioplayer.shufflePlaylist
 
-export const trackSelector = (store) => store.audioplayer.track
+export const trackSelector = (state) => state.audioplayer.track
 
-export const isTrackPlaySelector = (store) => store.audioplayer.playing
+export const isTrackPlaySelector = (state) => state.audioplayer.playing
